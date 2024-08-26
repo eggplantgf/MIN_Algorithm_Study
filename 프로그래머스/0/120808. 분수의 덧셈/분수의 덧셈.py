d88f62b9a1c25976b8d8f2648488aef6e0710181
MINJUNG KIM <95292485+eggplantgf@users.noreply.github.com>
2024-08-27 03:47:34 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -3,16 +3,10 @@ def gcd(a,b) :
         return a
     else :
         return gcd(b,a%b)
-    
-def lcm(a,b):
-    return (a*b)//gcd(a,b)
 
 def solution(numer1, denom1, numer2, denom2):
-    answer = []
-    LCM = lcm(denom1, denom2)
-    a = LCM//denom1*numer1 + LCM//denom2*numer2
-    b = LCM
-    if gcd(a,b) != 1:
+    a = numer1*denom2 + numer2*denom1
+    b = denom1*denom2
+    if gcd(a,b) != 1: # 기약분수 변경
         a, b = a//gcd(a,b) , b//gcd(a,b)
-    answer = [a,b]
-    return answer
+    return [a,b]
