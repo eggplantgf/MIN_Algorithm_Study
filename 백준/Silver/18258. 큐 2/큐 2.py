fca946816466bb4e83f11e06eff870813a75591b
MINJUNG KIM <95292485+eggplantgf@users.noreply.github.com>
2024-11-06 22:41:42 +0900

    [Silver IV] Title: 큐 2, Time: 1560 ms, Memory: 110552 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -0,0 +1,36 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+n = int(input().strip())
+
+arr = deque()  # 큐로 사용할 deque
+
+for i in range(n):
+    s = input().strip().split()
+    
+    # push
+    if s[0] == "push":
+        arr.append(int(s[1]))  # 큐에 정수 추가
+    
+    # pop
+    elif s[0] == "pop":
+        if arr:
+            print(arr.popleft())  # 큐에서 가장 앞의 정수를 제거하고 출력
+        else:
+            print(-1)
+
+    # size
+    elif s[0] == "size":
+        print(len(arr))  # 큐에 들어있는 정수의 개수를 출력
+
+    # empty
+    elif s[0] == "empty":
+        print(1 if not arr else 0)
+
+    # front
+    elif s[0] == "front":
+        print(arr[0] if arr else -1)  # 큐의 가장 앞에 있는 정수를 출력
+
+    # back
+    elif s[0] == "back":
+        print(arr[-1] if arr else -1)  # 큐의 가장 뒤에 있는 정수를 출력
