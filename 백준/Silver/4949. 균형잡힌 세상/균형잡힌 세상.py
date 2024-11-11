12de7c275944c7fec5afb275021dbdb70f775c31
MINJUNG KIM <95292485+eggplantgf@users.noreply.github.com>
2024-11-11 22:28:39 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 236 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -0,0 +1,27 @@
+while True :
+    arr = []
+
+    s = input()
+
+    if s == "." :
+        break
+
+    for i in s :
+        if i == '[' or i == '(' :
+            arr.append(i)
+        elif i == ']' :
+            if len(arr) != 0 and arr[-1] == '[' :
+                arr.pop()
+            else : 
+                arr.append(']')
+                break
+        elif i == ')' :
+            if len(arr) != 0 and arr[-1] == '(' :
+                arr.pop()
+            else :
+                arr.append(')')
+                break
+    if len(arr) == 0 :
+        print('yes')
+    else :
+        print('no')
