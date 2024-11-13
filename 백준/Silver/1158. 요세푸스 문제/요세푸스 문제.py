0416654b8a30f5b812c79cf0fd9ed1d7142b81f5
MINJUNG KIM <95292485+eggplantgf@users.noreply.github.com>
2024-11-13 23:51:07 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 1796 ms, Memory: 34016 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -0,0 +1,13 @@
+import sys
+from collections import deque
+
+n, k = map(int, input().split())
+arr = deque([i for i in range(1, n+1)])
+answer = []
+
+while len(arr) != 0:
+    for i in range(k-1):
+        arr.append(arr.popleft())
+    answer.append(str(arr.popleft()))
+
+print('<'+', '.join(answer)+'>')
